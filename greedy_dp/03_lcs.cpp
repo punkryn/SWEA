884afb798fe4226c4857f068f73f8f703e318978
punkryn <gns8167@naver.com>
2021-07-25 11:06:01 +0900

    update lcs

--- greedy_dp/03_lcs.cpp
+++ greedy_dp/03_lcs.cpp
@@ -5,50 +5,60 @@ using namespace std;
 
 char str[1001];
 char str2[1001];
-
+int dp[1001][1001];
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
 	
-	freopen("C:\\Users\\나\\Dropbox\\SWEA\\SWEA\\greedy_dp\\lcs.txt", "r", stdin);
+	freopen("C:\\Users\\gns81\\Dropbox\\SWEA\\SWEA\\greedy_dp\\lcs.txt", "r", stdin);
 	cin>>T;
 	/*
 	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 	*/
-    int idx, idx2;
-    int result;
-    int ans;
+    
+    int n, m;
+    
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
         cin >> str >> str2;
-        ans = 0;
-        idx = 0;
-        idx2 = 0;
-        result = 0;
-        for(int k = 0; k < strlen(str); k++){
-            for(int i = k; i < strlen(str); i++){
-                idx2 = idx;
-                while(idx2 < strlen(str2)){
-                    if(str2[idx2] == str[i]){
-                        result += 1;
-                        idx = idx2 + 1;
-                        break;
-                    }
-                    idx2++;
-                }
-            }
+        
+        n = strlen(str);
+        m = strlen(str2);
 
-            if(ans < result){
-                ans = result;
-            }
+        //cout << str << '\n' << str2 << '\n';
 
-            result = 0;
-            idx2 = 0;
-            idx = 0;
+        
+
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = 0;
         }
 
-        cout << '#' << test_case << ' ' << ans << '\n';
+        for(int i = 0; i <= m; i++){
+            dp[0][i] = 0;
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(str[i-1] == str2[j-1]){
+                    dp[i][j] = dp[i-1][j] > dp[i][j-1] ? (dp[i-1][j] > dp[i-1][j-1] + 1 ? dp[i-1][j] : dp[i-1][j-1] + 1) : (dp[i][j-1] > dp[i-1][j-1] + 1 ? dp[i][j-1] : dp[i-1][j-1] + 1);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] > dp[i][j-1] ? (dp[i-1][j] > dp[i-1][j-1] ? dp[i-1][j] : dp[i-1][j-1] ) : (dp[i][j-1] > dp[i-1][j-1] ? dp[i][j-1] : dp[i-1][j-1]);
+                }
+            }
+        }
+
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= m; j++){
+        //         printf("%d ", dp[i][j]);
+        //     }
+        //     printf("\n");
+        // }
+        // printf("\n");
+
+
+        cout << '#' << test_case << ' ' << dp[n][m] << '\n';
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
